b83718cd1bcf02973759ae069846d61e416dc59a
Sharif Faisal Ahmed <zhumon0@gmail.com>
2021-03-01 17:56:07 +0600

    Date Picker Fixed

--- src/Flight_Search.java
+++ src/Flight_Search.java
@@ -5,10 +5,15 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 public class Flight_Search {
+
+	static WebDriver driver;
+	static WebDriverWait wait;
 
 	// Selected button
 	public static void selected(List<WebElement> clS, String button) {
@@ -43,15 +48,68 @@ public class Flight_Search {
 		}
 	}
 
+	public static void datePicker(String date, String monthyear) {
+		boolean b = true;
+
+		while (b) {
+
+			WebElement group = driver
+					.findElement(By.cssSelector("[class='ui-datepicker-group ui-datepicker-group-first']"));
+
+			wait.until(ExpectedConditions
+					.visibilityOfAllElements(group.findElement(By.cssSelector(".ui-datepicker-title"))));
+
+			WebElement title = group.findElement(By.cssSelector(".ui-datepicker-title"));
+
+			// getting month text
+			String month = title.findElement(By.cssSelector(".ui-datepicker-month")).getText();
+
+			// getting year text
+			String year = title.findElement(By.cssSelector(".ui-datepicker-year")).getText();
+
+			// string concating to further compare
+			String monthYear1 = month.concat(year);
+
+			if (monthYear1.equalsIgnoreCase(monthyear)) {
+				wait.until(ExpectedConditions
+						.visibilityOfAllElements(group.findElements(By.cssSelector("[data-handler='selectDay']"))));
+				List<WebElement> dates = group.findElements(By.cssSelector("[data-handler='selectDay'] >a"));
+
+				for (WebElement day : dates) {
+					wait.until(ExpectedConditions.elementToBeClickable(day));
+					String current = day.getText();
+
+					if (current.equalsIgnoreCase(date)) {
+						day.click();
+						b = false;
+						break;
+
+					}
+				}
+
+			}
+
+			else {
+
+//				wait.until(ExpectedConditions.visibilityOfAllElements(driver.findElement(By.cssSelector("[data-handler='next']"))));
+				driver.findElement(By.cssSelector("[data-handler='next']")).click();
+
+			}
+
+		}
+
+	}
+
 	public static void main(String[] args) {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Demiurges\\Documents\\drivers\\chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 
 		driver.get("https://www.spicejet.com/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 5);
 
 		// actual title and urls
 		String url_actual = "https://www.spicejet.com/";
@@ -84,8 +142,12 @@ public class Flight_Search {
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='IXB']"))
 				.click();
 
-		// calender current date picking
-		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active")).click();
+		// calender start date picking
+		datePicker("22", "June2021");
+		
+		//calender return date picking
+		driver.findElement(By.id("ctl00_mainContent_view_date2")).click();
+		datePicker("7", "August2021");
 
 		// passenger dropdown selection
 		driver.findElement(By.id("divpaxinfo")).click();
@@ -107,6 +169,8 @@ public class Flight_Search {
 		// url assertion
 		String exp_url_booking = driver.getCurrentUrl();
 		Assert.assertEquals("https://book.spicejet.com/Select.aspx", exp_url_booking);
+		
+		driver.close();
 
 	}
 
